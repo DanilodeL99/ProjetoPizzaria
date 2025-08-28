725b4cb6f517d1dcfd6c5060f00da003feb799ec
2Amodular <2mod@bglab.local>
2025-08-28 14:33:07 -0300

    Criando valores no COMBOBOX

--- SistemaPizzaria/Pedido.cs
+++ SistemaPizzaria/Pedido.cs
@@ -12,6 +12,8 @@ namespace SistemaPizzaria
 {
     public partial class Pedido : Form
     {
+        //INSTANCIANDO A CLASSE DE CONEXÂO
+        Conexao con = new Conexao();
         public Pedido()
         {
             InitializeComponent();
@@ -20,6 +22,18 @@ namespace SistemaPizzaria
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pedido_Load(object sender, EventArgs e)
+        {
+            cmbTamanhoPizza.Items.Add("Pequena- R$ 20,00");
+            cmbTamanhoPizza.Items.Add("Média- R$ 30,00");
+            cmbTamanhoPizza.Items.Add("Grande- R$ 50,00");
         }
     }
 }
